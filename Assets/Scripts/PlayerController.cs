d6e6bfaed91047b8fbc25272470fcb1c18227b82
Hyundol <jackrkd022244@gmail.com>
2023-10-30 15:17:18 +0900

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.UI;
-
+//Å×½ºÆ®
 public class PlayerController : MonoBehaviour
 {
     public float speed;
