f790e5b429b0efd153e620baa3ca10200cfa1afe
yoonjjjang <dksdbswo00@gmail.com>
2023-10-31 23:12:10 +0900

    타이머 추가

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    //float elapsedTime; // 경과시간
+    [SerializeField] float remainingTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);  //경과시간 0초에서 증가하는 방식 */
+
+        if (remainingTime > 0) 
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if(remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+        }
+        if (remainingTime <= 30)
+        {
+            timerText.color = Color.yellow;
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
